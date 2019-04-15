39205890936b3b5fe85361720dc7debef95618fa
nikita123483 <316201@college.local>
2019-04-15 18:53:59 +0300

    Edit class Program

--- variant1/Program.cs
+++ variant1/Program.cs
@@ -10,6 +10,10 @@ namespace variant1
     {
         static void Main(string[] args)
         {
+            Account obj = new Account(); 
+            obj.Input();
+            Console.Write(obj);
+            Console.WriteLine();
         }
     }
 }
