312273731ccddb9f82f55e6b94690fb1b8145cc1
nikita123483 <316201@college.local>
2019-04-15 18:44:59 +0300

    Add class Account

---
+++ variant1/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace variant1
+{
+    class Account
+    {
+        public string Name { get; set; }
+        public int number { get; set; }
+        public double Summ { get; set; }
+        public Account() { }
+        public void Input()
+        {
+            Console.Write("Название счёта");
+            this.Name = Console.ReadLine();
+
+            Console.Write("номер счёта");
+            this.number = Convert.ToInt32( Console.ReadLine());
+
+            Console.Write("сумма счёта");
+            this.Summ = Convert.ToDouble(Console.ReadLine());
+        }
+        public override string ToString()
+        {
+            return string.Format("Название счёта {0} номер счёта {1} сумма счёта {2}", Name, number, Summ);
+        }
+    }
+}
